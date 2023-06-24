33ec0ec373437486c96f5374ec5b548149fe9ab2
Yassine Sz <0139ne@gmail.com>
2023-06-24 13:07:10 +0100

    Begin sprite batch

---
+++ Neon/Engine/Public/Renderer/Render/SpriteBatch.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <RHI/Resource/Resource.hpp>
+
+#include <Math/Vector.hpp>
+#include <Math/Rect.hpp>
+#include <Math/Colors.hpp>
+
+#include <vector>
+
+namespace Neon::Renderer
+{
+    class SpriteBatch
+    {
+    public:
+        struct QuadCommand
+        {
+            Vector2 Position;
+            Vector2 Size;
+            float   Rotation = 0.f;
+            float   Depth    = 0.f;
+            Color4  Color    = Colors::White;
+
+            Ptr<RHI::ITexture> Texture;
+            RectF              TexCoord = RectF(Vec::Zero<Vector2>, Vec::One<Vector2>);
+        };
+
+        /// <summary>
+        /// Enqueues a quad to be drawn.
+        /// </summary>
+        void Draw(
+            const QuadCommand& Quad);
+
+    private:
+        UPtr<RHI::IUploadBuffer> m_VertexBuffers;
+        UPtr<RHI::IUploadBuffer> m_IndexBuffer;
+    };
+} // namespace Neon::Renderer
